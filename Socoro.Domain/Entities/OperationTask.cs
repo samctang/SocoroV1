638f0e96f23f34d59f321885d91114ecce86a96d
samctang <samc.tang@gmail.com>
2021-08-29 14:36:05 -0700

    completed tasks and updated db

--- Socoro.Domain/Entities/OperationTask.cs
+++ Socoro.Domain/Entities/OperationTask.cs
@@ -6,7 +6,7 @@ namespace Socoro.Domain.Entities
     public class OperationTask : AuditableEntity
     {
         public string Note { get; set; }
-        public DateTime? DueDate { get; set; }
+        public DateTime DueDate { get; set; }
         public bool Status { get; set; }
         public int? OperationProcessId { get; set; }
         public OperationProcess OperationProcess { get; set; }
